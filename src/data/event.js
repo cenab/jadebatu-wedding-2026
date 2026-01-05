5dc6f27653df66f132cdc1f56d83003a3f5918a0
cenab <batu.bora389@gmail.com>
2026-01-04 17:39:18 -0700

    fixing sound

--- src/data/event.js
+++ src/data/event.js
@@ -1,29 +1,49 @@
 export const event = {
   coupleNames: "Uyen and Batu",
-  date: "Saturday, June 20, 2026",
-  dateISO: "2026-06-20",
+  date: "Sunday, Jan 18, 2026",
+  dateISO: "2026-01-18",
   startTime: "5:00 PM",
   endTime: "11:00 PM",
   startTime24: "17:00",
   endTime24: "23:00",
-  timezone: "America/New_York",
+  timezone: "America/Edmonton",
   venue: {
     name: "Rouge",
     address: "1240 8 Ave SE, Calgary, AB T2G 0M7, Canada",
     mapUrl: "https://maps.app.goo.gl/2dPLkABhheQ1Z2XEA"
   },
   dressCode: "Garden cocktail",
+  weddingParty: {
+    mc: "Doruk",
+    officiant: "Vadym",
+    bestMan: "Vadym",
+    maidOfHonor: "Darlyn"
+  },
   itinerary: [
-    { time: "5:00 PM", item: "Ceremony" },
-    { time: "5:30 PM", item: "Cocktail hour" },
-    { time: "6:30 PM", item: "Dinner" },
-    { time: "8:00 PM", item: "Dancing" }
+    { time: "5:00 PM", item: "Guest arrival & welcome" },
+    { time: "5:30 PM", item: "Ceremony begins" },
+    { time: "5:50 PM", item: "Cocktail hour" },
+    { time: "6:30 PM", item: "Dinner begins" },
+    { time: "6:35 PM", item: "Welcome toast" },
+    { time: "6:40 PM", item: "First course" },
+    { time: "7:05 PM", item: "Speech - Groom" },
+    { time: "7:15 PM", item: "Second course" },
+    { time: "7:40 PM", item: "Speech - Maid of Honor & Best Man" },
+    { time: "7:50 PM", item: "Third course" },
+    { time: "8:15 PM", item: "Speech - Father of the Groom" },
+    { time: "8:30 PM", item: "Cake cutting" },
+    { time: "8:40 PM", item: "Dessert" },
+    { time: "9:00 PM", item: "First dance" },
+    { time: "9:05 PM", item: "Music & dancing" },
+    { time: "10:30 PM", item: "Last call" },
+    { time: "10:50 PM", item: "Final song" },
+    { time: "11:00 PM", item: "Event ends" }
   ],
   notes: {
     parking: "Parking is available on-site. Please carpool if you can.",
     kids: "Adults only, thank you for understanding."
   },
-  rsvpDeadline: "May 30, 2026",
+  rsvpDeadline: "Jan 14, 2026",
   photo: {
     maxFileSizeMb: 20,
     maxFiles: 10,
